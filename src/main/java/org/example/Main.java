0ef56df926ffbabf2ac64d452e4232f56abb629f
mjosepc <mjose.powell@gmail.com>
2023-10-16 20:47:32 -0300

    2 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,12 +13,28 @@ class Persona {
         this.nombre = nombre;
         this.destino = destino;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public String getDestino() {
         return destino;
+    }
+}
+class Vehiculo {
+    private String tipo;
+    private double velocidadPromedio;
+
+    public Vehiculo(String tipo, double velocidadPromedio) {
+        this.tipo = tipo;
+        this.velocidadPromedio = velocidadPromedio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double calcularTiempo(int distancia) {
+        double tiempoHoras = (double) distancia / velocidadPromedio;
+        return tiempoHoras;
     }
 }
