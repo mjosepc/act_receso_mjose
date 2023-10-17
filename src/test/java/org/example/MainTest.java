bed202c6a925d5b18e7c15372a2b9b6c3c1ce728
mjosepc <mjose.powell@gmail.com>
2023-10-16 21:05:35 -0300

    prueba test commit

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,7 +1,37 @@
 package org.example;
-
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-class MainTest {
+class PersonaTest {
+    @Test
+    public void testGetNombre() {
+        Persona persona = new Persona("Juan", "el trabajo");
+        assertEquals("Juan", persona.getNombre());
+    }
 
+    @Test
+    public void testGetDestino() {
+        Persona persona = new Persona("Juan", "el trabajo");
+        assertEquals("el trabajo", persona.getDestino());
+    }
 }
+class VehiculoTest {
+
+        @Test
+        public void testCalcularTiempoAuto() {
+            Vehiculo auto = new Vehiculo("auto", 60.0);
+            assertEquals(1.0, auto.calcularTiempo(60)); // 60 km a 60 km/h = 1 hora
+        }
+
+        @Test
+        public void testCalcularTiempoMotocicleta() {
+            Vehiculo motocicleta = new Vehiculo("motocicleta", 80.0);
+            assertEquals(0.75, motocicleta.calcularTiempo(60)); // 60 km a 80 km/h = 0.75 horas
+        }
+
+        @Test
+        public void testCalcularTiempoBicicleta() {
+            Vehiculo bicicleta = new Vehiculo("bicicleta", 15.0);
+            assertEquals(4.0, bicicleta.calcularTiempo(60)); // 60 km a 15 km/h = 4 horas
+        }
+    }
